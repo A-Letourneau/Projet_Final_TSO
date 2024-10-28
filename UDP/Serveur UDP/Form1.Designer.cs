0d35a8547b968625c771b500d23f21ccb692f005
A-Letourneau <aleletourneau123@gmail.com>
2024-10-28 09:35:40 -0400

    Initial Commit

---
+++ UDP/Serveur UDP/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Projet_Final_Preparation_UDP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_IP_ADR = new System.Windows.Forms.TextBox();
+            this.textBox_Receive = new System.Windows.Forms.TextBox();
+            this.textBox_Send = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Confirm = new System.Windows.Forms.Button();
+            this.btn_listen = new System.Windows.Forms.Button();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_IP_ADR
+            // 
+            this.textBox_IP_ADR.Location = new System.Drawing.Point(73, 96);
+            this.textBox_IP_ADR.Name = "textBox_IP_ADR";
+            this.textBox_IP_ADR.Size = new System.Drawing.Size(192, 22);
+            this.textBox_IP_ADR.TabIndex = 0;
+            // 
+            // textBox_Receive
+            // 
+            this.textBox_Receive.Location = new System.Drawing.Point(528, 273);
+            this.textBox_Receive.Name = "textBox_Receive";
+            this.textBox_Receive.Size = new System.Drawing.Size(172, 22);
+            this.textBox_Receive.TabIndex = 1;
+            // 
+            // textBox_Send
+            // 
+            this.textBox_Send.Location = new System.Drawing.Point(528, 96);
+            this.textBox_Send.Name = "textBox_Send";
+            this.textBox_Send.Size = new System.Drawing.Size(172, 22);
+            this.textBox_Send.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(68, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Adresse IP esp32";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(523, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Send";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(523, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Receive";
+            // 
+            // btn_Confirm
+            // 
+            this.btn_Confirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btn_Confirm.Location = new System.Drawing.Point(106, 146);
+            this.btn_Confirm.Name = "btn_Confirm";
+            this.btn_Confirm.Size = new System.Drawing.Size(112, 43);
+            this.btn_Confirm.TabIndex = 6;
+            this.btn_Confirm.Text = "Confirm";
+            this.btn_Confirm.UseVisualStyleBackColor = true;
+            this.btn_Confirm.Click += new System.EventHandler(this.btn_Confirm_Click);
+            // 
+            // btn_listen
+            // 
+            this.btn_listen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btn_listen.Location = new System.Drawing.Point(560, 301);
+            this.btn_listen.Name = "btn_listen";
+            this.btn_listen.Size = new System.Drawing.Size(112, 43);
+            this.btn_listen.TabIndex = 7;
+            this.btn_listen.Text = "Listen";
+            this.btn_listen.UseVisualStyleBackColor = true;
+            this.btn_listen.Click += new System.EventHandler(this.btn_listen_Click);
+            // 
+            // btn_send
+            // 
+            this.btn_send.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btn_send.Location = new System.Drawing.Point(560, 124);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(112, 43);
+            this.btn_send.TabIndex = 8;
+            this.btn_send.Text = "Send";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.btn_listen);
+            this.Controls.Add(this.btn_Confirm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Send);
+            this.Controls.Add(this.textBox_Receive);
+            this.Controls.Add(this.textBox_IP_ADR);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_IP_ADR;
+        private System.Windows.Forms.TextBox textBox_Receive;
+        private System.Windows.Forms.TextBox textBox_Send;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Confirm;
+        private System.Windows.Forms.Button btn_listen;
+        private System.Windows.Forms.Button btn_send;
+    }
+}
+
---
+++ UDP/Serveur UDP/UPD_Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+using System.Threading;
+
+namespace ServerTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Program main = new Program();
+            main.server_start();  //starting the server
+
+            Console.ReadLine();
+        }
+
+        TcpListener server = new TcpListener(IPAddress.Any, 9999);
+
+        private void server_start()
+        {
+            server.Start();
+            accept_connection();  //accepts incoming connections
+        }
+
+        private void accept_connection()
+        {
+            server.BeginAcceptTcpClient(handle_connection, server);  //this is called asynchronously and will run in a different thread
+        }
+
+        private void handle_connection(IAsyncResult result)  //the parameter is a delegate, used to communicate between threads
+        {
+            accept_connection();  //once again, checking for any other incoming connections
+            TcpClient client = server.EndAcceptTcpClient(result);  //creates the TcpClient
+
+            NetworkStream ns = client.GetStream();
+
+            /* here you can add the code to send/receive data */
+
+        }
+
+    }
+}
